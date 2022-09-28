ec5f7ba40ae0ccea20516a11953c9a1f3230703b
weijiakang <416481873@qq.com>
2022-09-28 11:23:22 +0800

    master fourth conmit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git4");
+        System.out.println("hello git5");
+
     }
 
 }
